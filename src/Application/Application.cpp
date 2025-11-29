bb98c5e86fc0982146464ab697765729bb8db3d0
bardochman@student.agh.edu.pl <bardochman@student.agh.edu.pl>
2025-11-29 23:48:14 +0100

    WOJTEK: Prototyp renderowania 3D. Stworzenie klas Renderer3D, zmiany w Application - usunięcie osobnego okna z konsolą, dodanie okna imgui (tego niebiesko-czarnego) do wyświetlania obiektu. Zapoznanie się z draw_list, czyli tym w jaki sposób będziemy renderować wszystko. 29.11

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -1,36 +1,62 @@
 #include "Application.h"
 
-Application::Application(): Application(1200, 800, "Edit3D") {
+
+Application::Application() : Application(1200, 800, "Edit3D") {
 }
 
-Application::Application(unsigned int width, unsigned int height, sf::String title) 
-	:WINDOW_WIDTH{ width }, WINDOW_HEIGHT{ height }, WINDOW_TITLE{ title }, main_window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), WINDOW_TITLE), console1(sf::VideoMode({ 1200, 200 }),"Consola") {
+Application::Application(unsigned int width, unsigned int height, sf::String title)
+    : WINDOW_WIDTH{ width }, WINDOW_HEIGHT{ height }, WINDOW_TITLE{ title },
+    main_window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), WINDOW_TITLE)/*,
+    console_window(sf::VideoMode({ 1200, 200 }), "Consola")*/ {
+        //COMMENT: wyrzuci³em console_window bo nie jest narazie potrzebne
 
-	main_window.setFramerateLimit(FRAME_LIMIT);
-	main_window.clear(sf::Color::White);
-	std::ignore = ImGui::SFML::Init(main_window);
-	std::ignore = ImGui::SFML::Init(console1); // to tylko testowo raczej i tak najlepiej kosolê robiæ w nowym oknie ImGUI 
+    main_window.setFramerateLimit(FRAME_LIMIT);
+    std::ignore = ImGui::SFML::Init(main_window);
+    //std::ignore = ImGui::SFML::Init(console_window);
+	//COMMENT: wyrzuci³em console_window bo nie jest narazie potrzebne
 }
 
+void Application::handleEvents() {
+    while (const auto event = main_window.pollEvent()) {
+        ImGui::SFML::ProcessEvent(main_window, *event);
+
+        if (event->is<sf::Event::Closed>()) {
+            main_window.close();
+        }
+    }
+}
 
 void Application::run() {
-	sf::Clock deltaClock;
+    sf::Clock deltaClock;
 
-	while (main_window.isOpen()) {
-		while (const auto event = main_window.pollEvent()) {
-			ImGui::SFML::ProcessEvent(main_window, *event);
-			if (event->is<sf::Event::Closed>()) main_window.close();
-		}
-		ImGui::SFML::Update(main_window, deltaClock.restart());
+    while (main_window.isOpen()) {
+        handleEvents();
+        ImGui::SFML::Update(main_window, deltaClock.restart());
 
-		console.Draw();
+        main_window.clear(sf::Color::White);
 
-		main_window.clear(sf::Color::White);
-		ImGui::SFML::Render(main_window);
-		main_window.display();
+        // Rysowanie widoku 3D przez dedykowany renderer
+        renderer3D.Draw3DView();
 
+        // Panel kontrolny
+        ImGui::SetNextWindowSize(ImVec2(300, 200), ImGuiCond_FirstUseEver);
+        ImGui::SetNextWindowPos(ImVec2(700, 50), ImGuiCond_FirstUseEver);
 
+        ImGui::Begin("Kontrola 3D");
+        ImGui::SliderFloat("Obrot X", &renderer3D.rotationX, 0.0f, 360.0f);
+        ImGui::SliderFloat("Obrot Y", &renderer3D.rotationY, 0.0f, 360.0f);
+        ImGui::SliderFloat("Powiekszenie", &renderer3D.zoom, 0.1f, 3.0f);
+        if (ImGui::Button("Resetuj widok")) {
+            renderer3D.rotationX = 0.0f;
+            renderer3D.rotationY = 0.0f;
+            renderer3D.zoom = 1.0f;
+        }
+        ImGui::End();
 
-	}
-	ImGui::SFML::Shutdown();
+        console.Draw();
+
+        ImGui::SFML::Render(main_window);
+        main_window.display();
+    }
+    ImGui::SFML::Shutdown();
 }
--- src/PCH.h
+++ src/PCH.h
@@ -5,3 +5,15 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+//Rendering stuff
+#include <vector>
+#include <utility>
+#include <cmath>
+
+//COMMENT: IMGUI nie ma ImVec3, ale w ka¿dym projekcie jaki przejrza³em na githubie ludzie implementuj¹ swoj¹ wersjê,
+//wiêc wierzê ¿e nie sprawi to (mam nadziejê) za duzo problemów.
+struct ImVec3 {
+    float x, y, z;
+    ImVec3() noexcept : x(0.0f), y(0.0f), z(0.0f) {}
+    ImVec3(float _x, float _y, float _z) noexcept : x(_x), y(_y), z(_z) {}
+};

947eeb30350749e7203c959e51013ade11882c01
Maniek <mariusz2723@gmail.com>
2025-11-30 20:43:46 +0100

    Przesienie Render i Console do Scene

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -35,26 +35,7 @@ void Application::run() { // nominalnie w Application nie chcemy "suchego kodu" 
 
         main_window.clear(sf::Color::White);
 
-        // Rysowanie widoku 3D przez dedykowany renderer
-        renderer3D.Draw3DView();
-
-        // Panel kontrolny
-        ImGui::SetNextWindowSize(ImVec2(300, 200), ImGuiCond_FirstUseEver);
-        ImGui::SetNextWindowPos(ImVec2(700, 50), ImGuiCond_FirstUseEver);
-
-        ImGui::Begin("Kontrola 3D");
-            ImGui::SliderFloat("Obrot X", &renderer3D.rotationX, 0.0f, 360.0f); // czytelny format z wcieciem - Maniek
-            ImGui::SliderFloat("Obrot Y", &renderer3D.rotationY, 0.0f, 360.0f);
-            ImGui::SliderFloat("Powiekszenie", &renderer3D.zoom, 0.1f, 3.0f);
-            if (ImGui::Button("Resetuj widok")) {
-                renderer3D.rotationX = 0.0f;
-                renderer3D.rotationY = 0.0f;
-                renderer3D.zoom = 1.0f;
-            }
-        ImGui::End();
-
-
-        console.Draw();
+        scene.drawScene();
 
         ImGui::SFML::Render(main_window);
         main_window.display();
